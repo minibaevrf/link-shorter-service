0c48a71ca9dfe1c740a8e72db84453f33cada63e
Rinat Minibaev <v-rimini@microsoft.com>
2020-04-07 19:00:31 +0300

    Logging fix

--- logger.py
+++ logger.py
@@ -1,13 +1,17 @@
 import logging
+import os
 
 
 class Logger:
 
     @staticmethod
     def create_logger():
+        log_filename = 'log/logs.log'
+        os.makedirs(os.path.dirname(log_filename), exist_ok=True)
+
         logger = logging.getLogger(__name__)
         logger.setLevel(logging.DEBUG)
-        fh = logging.FileHandler(r'log//logs.log')
+        fh = logging.FileHandler(log_filename)
         fh.setLevel(logging.DEBUG)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         fh.setFormatter(formatter)
